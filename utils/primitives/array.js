e795b93e6e7070a124c06d017979a101a4246b3b
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-02-20 20:08:27 +0100

    Updated: eslint errors.

--- utils/primitives/array.js
+++ utils/primitives/array.js
@@ -34,14 +34,20 @@ exports.arrRemove = function(arr, fn, v) { // FROM TOTAL.JS
     var tmp = [];
     for (var i = 0, len = arr.length; i < len; i++) {
         if (isFN) {
-            !fn.call(arr, arr[i], i) && tmp.push(arr[i]);
+            if (!fn.call(arr, arr[i], i)) {
+                tmp.push(arr[i]);
+            }
             continue;
         }
         if (isV) {
-            arr[i] && arr[i][fn] !== v && tmp.push(arr[i]);
+            if (arr[i] && arr[i][fn] !== v) {
+                tmp.push(arr[i]);
+            }
             continue;
         }
-        arr[i] !== fn && tmp.push(arr[i]);
+        if (arr[i] !== fn) {
+            tmp.push(arr[i]);
+        }
     }
     return tmp;
 };
@@ -79,44 +85,72 @@ exports.arrOrderBy = function(arr, name, asc, maxlength) { // FROM TOTAL.JS EXCE
             type = 3;
             break;
         default:
-            if (!field instanceof Date && !isNaN(field.getTime())) {
+            if (!(field instanceof Date) && !isNaN(field.getTime())) {
                 return arr;
             }
             type = 4;
             break;
     }
-    function _shellInsertionSort(list, length, gapSize, fn) {
-        var temp, i, j;
-        for (i = gapSize; i < length; i += gapSize ) {
+    function shellInsertionSort(list, length, gapSize, fn) {
+        var i = 0;
+        var j = 0;
+        var temp = null;
+        for (i = gapSize; i < length; i += gapSize) {
             j = i;
-            while(j > 0 && fn(list[j - gapSize], list[j]) === 1) {
+            while (j > 0 && fn(list[j - gapSize], list[j]) === 1) {
                 temp = list[j];
                 list[j] = list[j - gapSize];
                 list[j - gapSize] = temp;
                 j -= gapSize;
             }
         }
-    };
+    }
     function shellsort(arr, fn) {
         var length = arr.length;
         var gapSize = Math.floor(length / 2);
-        while(gapSize) {
-            _shellInsertionSort(arr, length, gapSize, fn);
+        while (gapSize) {
+            shellInsertionSort(arr, length, gapSize, fn);
             gapSize = Math.floor(gapSize / 2);
         }
         return arr;
-    };
+    }
     shellsort(arr, function(a, b) {
         var va = name ? a[name] : a;
         var vb = name ? b[name] : b;
         if (type === 1) {
-            return (va && vb) ? (asc ? exports.strRemoveDiacritics(va.substring(0, maxlength)).localeCompare(exports.strRemoveDiacritics(vb.substring(0, maxlength))) : exports.strRemoveDiacritics(vb.substring(0, maxlength)).localeCompare(exports.strRemoveDiacritics(va.substring(0, maxlength)))) : 0;
+            if (va && vb) {
+                if (asc) {
+                    return exports.strRemoveDiacritics(va.substring(0, maxlength)).localeCompare(exports.strRemoveDiacritics(vb.substring(0, maxlength)));
+                }
+                else {
+                    return exports.strRemoveDiacritics(vb.substring(0, maxlength)).localeCompare(exports.strRemoveDiacritics(va.substring(0, maxlength)));
+                }
+            }
+            else {
+                return 0;
+            }
         }
         else if (type === 2) {
-            return va > vb ? (asc ? 1 : -1) : va < vb ? (asc ? -1 : 1) : 0;
+            if (va > vb) {
+                return asc ? 1 : -1;
+            }
+            else if (va < vb) {
+                return asc ? -1 : 1;
+            }
+            else {
+                return 0;
+            }
         }
         else if (type === 3) {
-            return va === true && vb === false ? (asc ? 1 : -1) : va === false && vb === true ? (asc ? -1 : 1) : 0;
+            if (va === true && vb === false) {
+                return asc ? 1 : -1;
+            }
+            else if (va === false && vb === true) {
+                return asc ? -1 : 1;
+            }
+            else {
+                return 0;
+            }
         }
         else if (type === 4) {
             if (!va || !vb) {
@@ -130,9 +164,19 @@ exports.arrOrderBy = function(arr, name, asc, maxlength) { // FROM TOTAL.JS EXCE
             }
             var at = va.getTime();
             var bt = vb.getTime();
-            return at > bt ? (asc ? 1 : -1) : at < bt ? (asc ? -1 : 1) : 0;
+            if (at > bt) {
+                return asc ? 1 : -1;
+            }
+            else if (at < bt) {
+                return asc ? -1 : 1;
+            }
+            else {
+                return 0;
+            }
         }
-        return 0;
+        else {
+            return 0;
+        }
     });
     return arr;
 };
@@ -145,7 +189,9 @@ exports.arrUnique = function(arr, k) {
     for (var i = 0, len = arr.length; i < len; i++) {
         var v = arr[i];
         if (!k) {
-            result.indexOf(v) === -1 && result.push(v);
+            if (result.indexOf(v) === -1) {
+                result.push(v);
+            }
             continue;
         }
         if (sublen === 0) {
