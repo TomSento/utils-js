0d67f0d5356cb79c7fb1b59f24afc934f19bb9fe
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-03-05 00:22:48 +0100

    Update: refactored.

--- utils/browser/route0.js
+++ utils/browser/route0.js
@@ -9,6 +9,8 @@
 // NO SUPPORT FOR BASEURL - DEVELOPER MUST ALWAYS PROVIDE FULL URL
 // SUPPORTS ONLY GET METHOD
 // LOCAL STORAGE VS. COOKIE - https://stackoverflow.com/a/3220802/6135126
+// REGEX TO MATCH ROUTE TAKEN FROM: https://github.com/garygreen/lightrouter
+// DOMContentLoaded VS. load - https://stackoverflow.com/a/36096571/6135126
 exports.SETROUTE0 = function(route, fn) {
     var cache = exports.malloc('_ROUTE0');
     if (!route || typeof(route) !== 'string' || route[0] !== '#') {
@@ -37,16 +39,11 @@ exports.SETROUTE0 = function(route, fn) {
             fn: fn
         };
     }
-    function isErrorRoute(route) {
-        return ['#400', '#404', '#408', '#500'].indexOf(route) >= 0;
+    function isErrorRoute(v) {
+        return /^#(400|404|408|500)/.test(v);
     }
     function domReady(fn) {
-        if (document.attachEvent ? (document.readyState === 'complete') : (document.readyState !== 'loading')) {
-            fn();
-        }
-        else {
-            document.addEventListener('DOMContentLoaded', fn);
-        }
+        document.addEventListener('DOMContentLoaded', fn);
     }
 };
 exports.ROUTE0 = function(url, err) {
@@ -55,65 +52,62 @@ exports.ROUTE0 = function(url, err) {
     if (!Array.isArray(routes) || routes.length === 0) {
         throw new Error('missingRoute');
     }
+    if (url !== undefined && typeof(url) !== 'string') {
+        throw new Error('invalidParameter');
+    }
     if (typeof(url) === 'string' && url[0] !== '#') {
         throw new Error('invalidParameter');
     }
-    var hash = locationHash();
-    console.log('HASH: ', hash);
-    console.log('ROUTES: ', cache('routes'));
+    if (!url && err) {
+        throw new Error('invalidParameter');
+    }
+    var hash = [null, locationHash()];
     if (!window.onhashchange) {
         window.onhashchange = function() {
+            hash[0] = hash[1];
+            hash[1] = locationHash();
             onHashChange(hash);
-            hash = locationHash();
         };
     }
-    var route = parseRoute(url);
-    console.log('FINDROUTE: ', route);
-    var found = false;
-    for (var i = 0; i < routes.length; i++) {
-        var v = routes[i];
-        if (v.exp.test(route.path)) {
-            found = true;
-            console.log('URL_FOUND');
-            var args = route.path.match(v.exp).slice(1);
-            var anchor = v.hasAnchor ? (args.pop() || null) : null; // FORCE TO NULL IF URL IS LIKE #users~
-            err = isErrorRoute(route.path) ? getErrorBuilder(err) : null;
-            console.log('ERR: ', err);
-            console.log('ARGS: ', args);
-            console.log('ANCHOR: ', anchor);
-            var controller = new exports.Controller0(v.route, route.query, args, anchor, err);
-            console.log('FN: ', v.fn.toString());
-            invokeRoute(v.fn, controller, args);
-            if (url || (hash.length === 1 && hash[0] === '#')) {
-                console.log('REPLACE:', route.url);
-                location.replace(route.url); // UPDATE ADDRESSBAR URL TO #ERR_ROUTE. UPDATE ADDRESSBAR FROM # TO ''
-            }
+    if (!url && !err) { // WHEN PAGE IS LOADED - REGISTERED IN SETROUTE0
+        err = localStorage['ROUTE0_ERR'];
+        err = isErrorRoute(location.hash)
+            ? new exports.ErrorBuilder(err ? JSON.parse(err) : null)
+            : null;
+        var search = location.hash.split(/\?+/)[0] || '#';
+        var query = getQueryObj(location.hash);
+        return findAndInvokeRoute(routes, search, query, err);
+    }
+    else {
+        if (isErrorRoute(url)) {
+            localStorage['ROUTE0_ERR'] = err ? JSON.stringify(new exports.ErrorBuilder(err)) : '';
+            location.replace(url); // FORGET HISTORY
+        }
+        else {
+            location.assign(url === '#' ? '' : url); // REMEMBER HISTORY
         }
     }
-    if (!found) {
-        console.log('URL_NOTFOUND');
-        exports.ROUTE0('#404');
+    function findAndInvokeRoute(routes, search, query, err) {
+        var is = false;
+        for (var i = 0; i < routes.length; i++) {
+            var v = routes[i];
+            if (v.exp.test(search)) {
+                is = true;
+                var args = search.match(v.exp).slice(1);
+                var anchor = v.hasAnchor ? (args.pop() || null) : null; // FORCE TO NULL IF URL IS #users~
+                var controller = new exports.Controller0(v.route, query, args, anchor, err);
+                return invokeRoute(v.fn, controller, args);
+            }
+        }
+        if (!is) {
+            exports.ROUTE0('#404');
+        }
     }
     function invokeRoute(fn, controller, args) {
         controller.startInterval();
         return processFlags(function() {
             fn.apply(controller, args);
         });
-    }
-    function getErrorBuilder(err) {
-        err = [err, localStorage['ROUTE0_ERR']];
-        if (err[0]) {
-            console.log('OK1');
-            return new exports.ErrorBuilder(err[0]);
-        }
-        else if (err[1]) {
-            console.log('OK2: ', err[1]);
-            return new exports.ErrorBuilder(JSON.parse(err[1]));
-        }
-        else {
-            console.log('OK3');
-            return new exports.ErrorBuilder();
-        }
     }
     function processFlags(next) {
         return processMiddlewares(function() {
@@ -122,16 +116,6 @@ exports.ROUTE0 = function(url, err) {
     }
     function processMiddlewares(next) {
         return next();
-    }
-    function parseRoute(url) {
-        var path = url ? url.split(/\?+/g)[0] : locationHash().split(/\?+/g)[0];
-        url = url ? url : location.hash;
-        url = url === '#' ? '' : url;
-        return {
-            url: url, // FOR ADDRESSBAR
-            path: path || '#', // ONLY PATH WITHOUT QUERY STRING
-            query: getQueryObj(url)
-        };
     }
     function getQueryObj(url) {
         url = url.replace(/\+/g, ' ');
@@ -150,37 +134,26 @@ exports.ROUTE0 = function(url, err) {
         return typeof(hash) === 'string' ? hash : '';
     }
     function onHashChange(hash) {
-        console.log('HASH:', hash);
-        console.log('LASH:', locationHash());
-        var url = [
-            hash.split('~'),
-            locationHash().split('~')
+        var part = [
+            hash[0].split('~'),
+            hash[1].split('~')
         ];
-        // if (isErrorRoute(url[1][0])) {
-        //     if (url[1][1] && url[0][1] !== url[1][1]) { // ANCHOR WAS AND IS SETTED, BUT VALUE WAS CHANGED
-        //         return scrollToID(url[1][1]);
-        //     }
-        // }
-        // else {
-
-        // if (url[0][0] === url[1][0]) {
-        //     if (url[0][1] && !url[1][1]) { // ANCHOR WAS PRESENT BUT USER REMOVED ANCHOR AND HITTED ENTER IN ADDRESSBAR
-        //         return replaceAndReload(locationHash());
-        //     }
-        //     else if (url[1][1] && url[0][1] !== url[1][1]) { // ANCHOR WAS AND IS SETTED, BUT VALUE WAS CHANGED
-        //         return scrollToID(url[1][1]);
-        //     }
-        // }
-        // else {
-        //     return replaceAndReload(locationHash()); // WE MUST TO RELOAD WHOLE PAGE OTHERWISE ASYNC SCRIPTS CAN INSERT DATA TO PAGE FOR WHICH THEY WERE NOT INTENDED.
-        // }
-
-        // }
-    }
-    function replaceAndReload(url) {
-        console.log('REPLACE_RELOAD');
-        location.replace(url);
-        return location.reload();
+        if (part[0][0] === part[1][0]) {
+            if (part[0][1] && !part[1][1]) { // ANCHOR WAS PRESENT BUT USER REMOVED ANCHOR AND HITTED ENTER IN ADDRESSBAR
+                location.reload();
+            }
+            else if (part[1][1] && part[0][1] !== part[1][1]) { // ANCHOR WAS AND IS PRESENT, BUT VALUE WAS CHANGED
+                return scrollToID(part[1][1]);
+            }
+        }
+        else {
+            if (part[1][0] === '#') {
+                location.replace(''); // PREVENT DOUBLE RELOAD WHEN REDIRECTING FROM # TO ''. FORGET '#' HISTORY.
+            }
+            else {
+                return location.reload();
+            }
+        }
     }
     function scrollToID(elementID) {
         var el = document.getElementById(elementID);
@@ -188,8 +161,8 @@ exports.ROUTE0 = function(url, err) {
             el.scrollIntoView();
         }
     }
-    function isErrorRoute(route) {
-        return ['#400', '#404', '#408', '#500'].indexOf(route) >= 0;
+    function isErrorRoute(v) {
+        return /^#(400|404|408|500)/.test(v);
     }
 };
 function Controller0(route, query, args, anchor, err) {
@@ -203,48 +176,46 @@ function Controller0(route, query, args, anchor, err) {
     self.execTime = 0;
     self.startInterval = function() {
         self.interval = setInterval(function() {
-            self.execTime += 500;
+            self.execTime += 1000;
             if (self.execTime >= 10000) {
                 clearInterval(self.interval);
                 return self.throw408();
             }
-        }, 500);
+        }, 1000);
     };
 }
 Controller0.prototype = {
     view: function(html) { // SETS PAGE HTML
         var self = this;
+        if (self.execTime >= 10000) {
+            return;
+        }
+        if (html !== 'string') {
+            throw new Error('invalidParameter');
+        }
         if (self.viewCalled) {
-            throw new Error("Multiple 'view()' invocations.");
+            throw new Error('invocationCount');
         }
         self.viewCalled = true;
-        clearInterval(self.interval);
-        if (self.execTime >= 10000) {
-            return self.throw408();
-        }
         var el = document.getElementById('page');
         if (el) {
-            el.innerHTML = html || '';
+            el.innerHTML = html;
         }
         else {
             throw new Error('Element #page was not found.');
         }
     },
     throw400: function(err) {
-        localStorage['ROUTE0_ERR'] = err ? JSON.stringify(new exports.ErrorBuilder(err)) : '';
-        exports.ROUTE0('#400');
+        exports.ROUTE0('#400', err);
     },
     throw404: function(err) {
-        localStorage['ROUTE0_ERR'] = err ? JSON.stringify(new exports.ErrorBuilder(err)) : '';
-        exports.ROUTE0('#404');
+        exports.ROUTE0('#404', err);
     },
     throw408: function(err) {
-        localStorage['ROUTE0_ERR'] = err ? JSON.stringify(new exports.ErrorBuilder(err)) : '';
-        exports.ROUTE0('#408');
+        exports.ROUTE0('#408', err);
     },
     throw500: function(err) {
-        localStorage['ROUTE0_ERR'] = err ? JSON.stringify(new exports.ErrorBuilder(err)) : '';
-        exports.ROUTE0('#500');
+        exports.ROUTE0('#500', err);
     }
 };
 exports.Controller0 = Controller0;
