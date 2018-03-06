05fcfbedaca8cb94fb7ff274516e84b07e888280
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-03-06 12:36:42 +0100

    Fixed: strToQueryObj().

--- utils/primitives/string.js
+++ utils/primitives/string.js
@@ -1092,13 +1092,15 @@ exports.strMatchLen = function(str, regex, i) {
 };
 exports.strToQueryObj = function(url) { // BASED ON: https://stackoverflow.com/a/32354921/6135126
     url = url.replace(/\+/g, ' ');
-    var exp = /[?&]([^=#]+)=([^&#]*)/g;
+    var exp = /[?&]([^=]+)=([^&]*)/g;
     var o = {};
     var m = null;
     while (m = exp.exec(url)) {
-        var k = decodeURIComponent(m[1]);
-        var v = decodeURIComponent(m[2]);
-        o[k] = v;
+        var k = m[1] ? decodeURIComponent(m[1]) : '';
+        var v = m[2] ? decodeURIComponent(m[2]) : '';
+        if (k) {
+            o[k] = v;
+        }
     }
     return o;
 };
