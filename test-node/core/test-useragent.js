3f489cb5bb66e6d42f9da026b78bc57688e16997
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-04-21 12:23:01 +0200

    Added: useragent "engines" test.

--- test-node/core/test-useragent.js
+++ test-node/core/test-useragent.js
@@ -1,5 +1,6 @@
 var U = require('../../dist/utils.git');
 var browsers = require('./files/test-useragent_browsers.json'); // FROM "ua-parser 0.7.17"
+var engines = require('./files/test-useragent_engines.json');
 
 U.arrForEach(browsers, function(test) {
     U.test('(browser) ' + test.desc, function(assert) {
@@ -10,13 +11,22 @@ U.arrForEach(browsers, function(test) {
     });
 });
 
-// browsers = U.arrMap(browsers, function(test) {
+U.arrForEach(engines, function(test) {
+    U.test('(engine) ' + test.desc, function(assert) {
+        var v = U.userAgent(test.ua).engineName;
+        assert.ok(v === test.expect.name, v, test.expect.name, '(engine/name) ' + test.desc);
+        v = U.userAgent(test.ua).engineVersion;
+        assert.ok(v === test.expect.version, v, test.expect.version, '(engine/version) ' + test.desc);
+    });
+});
+
+// engines = U.arrMap(engines, function(test) {
 //     test.expect.name = strToSnakeCase(test.expect.name);
 //     test.expect.version = strToSnakeCase(test.expect.version);
 //     return test;
 // });
 
-// require('fs').writeFileSync('./files/test-useragent_browsers-processed.json', JSON.stringify(browsers, null, '    '));
+// require('fs').writeFileSync('./files/test-useragent_engines-processed.json', JSON.stringify(engines, null, '    '));
 
 // function strToSnakeCase(str) {
 //     if (typeof(str) === 'string') {
