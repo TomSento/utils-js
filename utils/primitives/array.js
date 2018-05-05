60042ac9054a9a282a6f554a2bc0a6854313e60b
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-05-05 17:00:00 +0200

    Updated: arrMap() now supports also mapping directly by key if second argument is not a function.

--- utils/primitives/array.js
+++ utils/primitives/array.js
@@ -7,7 +7,13 @@ exports.arrForEach = function(arr, fn) {
 exports.arrMap = function(arr, fn) {
     var acc = [];
     for (var i = 0; i < arr.length; i++) {
-        acc.push(fn(arr[i], i, arr));
+        var v = arr[i];
+        if (typeof(fn) === 'function') {
+            acc.push(fn(v, i, arr));
+        }
+        else {
+            acc.push(v && typeof(v) === 'object' ? v[fn] : undefined);
+        }
     }
     return acc;
 };
