1b0bedb0b23caba80438422217391ffa8c3a166c
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-06-22 15:26:26 +0200

    Updated: to $ prefixed fns - "tests/primitives/str.js".

--- tests/primitives/str.js
+++ tests/primitives/str.js
@@ -1,4 +1,4 @@
-var U = require('../../dist/utils.git.js');
+require('../../dist/utils.all.js');
 
 var strA = 'aa  aa';
 var strB = ' aa   aa ';
@@ -8,23 +8,23 @@ var strE = '    aa      aa    ';
 
 // TOTAL START NBPS
 // TOTAL END NBPS
-U.log('1A:' + U.strToHTMLText(strA, true, false, true, true).replace(/\s/g, '_'));
-U.log('1B:' + U.strToHTMLText(strB, true, false, true, true).replace(/\s/g, '_'));
-U.log('1C:' + U.strToHTMLText(strC, true, false, true, true).replace(/\s/g, '_'));
-U.log('1D:' + U.strToHTMLText(strD, true, false, true, true).replace(/\s/g, '_'));
-U.log('1E:' + U.strToHTMLText(strE, true, false, true, true).replace(/\s/g, '_'));
+$log('1A:' + $strToHTMLText(strA, true, false, true, true).replace(/\s/g, '_'));
+$log('1B:' + $strToHTMLText(strB, true, false, true, true).replace(/\s/g, '_'));
+$log('1C:' + $strToHTMLText(strC, true, false, true, true).replace(/\s/g, '_'));
+$log('1D:' + $strToHTMLText(strD, true, false, true, true).replace(/\s/g, '_'));
+$log('1E:' + $strToHTMLText(strE, true, false, true, true).replace(/\s/g, '_'));
 
 // *a WHERE * IS BREAKING SPACE
 // TOTAL END BREAKING SPACE
-U.log('2A:' + U.strToHTMLText(strA, false, true, false, true).replace(/\s/g, '_'));
-U.log('2B:' + U.strToHTMLText(strB, false, true, false, true).replace(/\s/g, '_'));
-U.log('2C:' + U.strToHTMLText(strC, false, true, false, true).replace(/\s/g, '_'));
-U.log('2D:' + U.strToHTMLText(strD, false, true, false, true).replace(/\s/g, '_'));
-U.log('2E:' + U.strToHTMLText(strE, false, true, false, true).replace(/\s/g, '_'));
+$log('2A:' + $strToHTMLText(strA, false, true, false, true).replace(/\s/g, '_'));
+$log('2B:' + $strToHTMLText(strB, false, true, false, true).replace(/\s/g, '_'));
+$log('2C:' + $strToHTMLText(strC, false, true, false, true).replace(/\s/g, '_'));
+$log('2D:' + $strToHTMLText(strD, false, true, false, true).replace(/\s/g, '_'));
+$log('2E:' + $strToHTMLText(strE, false, true, false, true).replace(/\s/g, '_'));
 
 // ETC...
 
-var reversed = U.strReverse('a   bcd');
-U.log('reversed:' + reversed);
-var until = U.strUntil(reversed, /\s+/g);
-U.log('until:' + until);
+var reversed = $strReverse('a   bcd');
+$log('reversed:' + reversed);
+var until = $strUntil(reversed, /\s+/g);
+$log('until:' + until);
