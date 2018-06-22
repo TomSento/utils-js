4dab63b4ce28023dcc76911f3778ba38921d2fc4
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-06-22 14:42:51 +0200

    Updated: to $ prefixed fns - "tests/core/errors.js".

--- tests/core/errors.js
+++ tests/core/errors.js
@@ -1,14 +1,14 @@
-var U = require('../../dist/utils.git.js');
+require('../../dist/utils.all.js');
 
-var errorBuilder = new U.ErrorBuilder();
-var err = new U.Error('BuggsBunny-name', 'Parameter ...');
+var errorBuilder = new $ErrorBuilder();
+var err = new $Error('BuggsBunny-name', 'Parameter ...');
 // err.throw();
-U.log(err.toString());
-// new U.Error('invalidParameter').logAndThrow();
-errorBuilder.push(new U.Error('BuggsBunny-name', 'Parameter "name" is missing or has incorrect format.'));
-errorBuilder.push(new U.Error(new Error('BuggsBunny-carrots'), 'Parameter "carrots" is missing or has incorrect format.'));
-U.log('Builder0: ', errorBuilder.toString());
+$log(err.toString());
+// new $Error('invalidParameter').logAndThrow();
+errorBuilder.push(new $Error('BuggsBunny-name', 'Parameter "name" is missing or has incorrect format.'));
+errorBuilder.push(new $Error(new Error('BuggsBunny-carrots'), 'Parameter "carrots" is missing or has incorrect format.'));
+$log('Builder0: ', errorBuilder.toString());
 errorBuilder.remove('BuggsBunny-carrots');
-U.log('Builder1: ', errorBuilder.toString());
+$log('Builder1: ', errorBuilder.toString());
 errorBuilder.clear();
-U.log('Builder2: ', errorBuilder.toString());
+$log('Builder2: ', errorBuilder.toString());
