544bdab1ed44314e37a934183ed2b0728201a5f3
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-06-29 12:07:06 +0200

    Added: dom imports to utils.browser.js source.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,5 +1,38 @@
 import './utils.common';
 
+import '../utils2/4_browser/dom/domAppend';
+import '../utils2/4_browser/dom/domAttr';
+import '../utils2/4_browser/dom/domChildren';
+import '../utils2/4_browser/dom/domData';
+import '../utils2/4_browser/dom/domFadeIn';
+import '../utils2/4_browser/dom/domFadeOut';
+import '../utils2/4_browser/dom/domFadeTo';
+import '../utils2/4_browser/dom/domFadeToggle';
+import '../utils2/4_browser/dom/domFind';
+import '../utils2/4_browser/dom/domHasClass';
+import '../utils2/4_browser/dom/domHTML';
+import '../utils2/4_browser/dom/domIsEl';
+import '../utils2/4_browser/dom/domMatches';
+import '../utils2/4_browser/dom/domNext';
+import '../utils2/4_browser/dom/domNextAll';
+import '../utils2/4_browser/dom/domOff';
+import '../utils2/4_browser/dom/domOn';
+import '../utils2/4_browser/dom/domParent';
+import '../utils2/4_browser/dom/domPrepend';
+import '../utils2/4_browser/dom/domPrev';
+import '../utils2/4_browser/dom/domPrevAll';
+import '../utils2/4_browser/dom/domReady';
+import '../utils2/4_browser/dom/domRemove';
+import '../utils2/4_browser/dom/domRemoveClass';
+import '../utils2/4_browser/dom/domScrollTo';
+import '../utils2/4_browser/dom/domSiblings';
+import '../utils2/4_browser/dom/domStyle';
+import '../utils2/4_browser/dom/domText';
+import '../utils2/4_browser/dom/domToggleClass';
+import '../utils2/4_browser/dom/domTriggerEvent';
+import '../utils2/4_browser/dom/domTriggerNativeEvent';
+import '../utils2/4_browser/dom/domVal';
+
 import '../utils2/4_browser/router/0_route0';
 import '../utils2/4_browser/router/1_Controller0';
 
