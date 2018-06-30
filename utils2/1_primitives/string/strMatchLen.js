4b0cc2c252757e35011336f2bbc04b7ba936f1a4
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-06-30 11:38:14 +0200

    Updated: $strMatchLen() argument name - no logic change.

--- utils2/1_primitives/string/strMatchLen.js
+++ utils2/1_primitives/string/strMatchLen.js
@@ -1,7 +1,7 @@
 import $global from '../../global';
 
-export default function $strMatchLen(str, regex, i) {
-    var matches = str.match(regex);
+export default function $strMatchLen(str, exp, i) {
+    var matches = str.match(exp);
     i = parseInt(i);
     i = isNaN(i) ? 0 : i;
     return (Array.isArray(matches) && matches[i]) ? matches[i].length : 0;
