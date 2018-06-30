fe8910d5123340855dd8224cfb580e9261840850
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-06-30 14:17:46 +0200

    Updated: objExtend() arguments validation.

--- utils2/1_primitives/object/objExtend.js
+++ utils2/1_primitives/object/objExtend.js
@@ -1,24 +1,24 @@
 import $global from '../../global';
 import $objClone from './objClone';
 
-export default function $objExtend(base, obj, rewrite) {
-    if (!base || !obj) {
-        return base;
+export default function $objExtend(objA, objB, rewrite) {
+    if (!objA || typeof(objA) !== 'object') {
+        throw new Error('api-objA');
     }
-    if (typeof(base) !== 'object' || typeof(obj) !== 'object') {
-        return base;
+    if (!objB || typeof(objB) !== 'object') {
+        throw new Error('api-objB');
     }
     if (rewrite === undefined) {
         rewrite = true;
     }
-    var keys = Object.keys(obj);
+    var keys = Object.keys(objB);
     var i = keys.length;
     while (i--) {
         var key = keys[i];
-        if (rewrite || base[key] === undefined) {
-            base[key] = $objClone(obj[key]);
+        if (rewrite || objA[key] === undefined) {
+            objA[key] = $objClone(objB[key]);
         }
     }
-    return base;
+    return objA;
 }
 $global.$objExtend = $objExtend;
