b3d704ca4db9eb32a38fd4f03668c8dc4cca6694
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-03 20:32:09 +0200

    Updated: separated dom utils.

--- utils2/4_browser/dom/domChildren.js
+++ utils2/4_browser/dom/domChildren.js
@@ -1,31 +1,18 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import { $toArrayOfElements, $selectingOne } from './_utils';
 
 export default function $domChildren(sel) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
+    var els = $toArrayOfElements(sel);
     var arr = [];
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                arr.push(getChildren(el));
-            }
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            arr.push(getChildren(el));
         }
     }
-    return selectingOne(sel) ? arr[0] : arr;
+    return $selectingOne(sel) ? arr[0] : arr;
     function getChildren(el) {
         var els = el.children || [];
         var arr = [];
@@ -33,16 +20,6 @@ export default function $domChildren(sel) {
             arr[i] = els[i];
         }
         return arr;
-    }
-    function selectingOne(sel) {
-        if ($domIsEl(sel)) {
-            return true;
-        }
-        else if (typeof(sel) === 'string') {
-            var parts = sel.split(/\s+/);
-            return (parts && parts.length == 1 && parts[0][0] == '#');
-        }
-        return false;
     }
 }
 window.$domChildren = $domChildren;
