b6480dcbabe5ff4244d9b726342d5f44206896b2
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-04 11:14:15 +0200

    Updated: separated dom utils.

--- utils2/4_browser/dom/domFadeTo.js
+++ utils2/4_browser/dom/domFadeTo.js
@@ -1,5 +1,4 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import { $toArrayOfElements } from './_utils';
 
 export default function $domFadeTo(sel, o, t) {
     if (!sel) {
@@ -10,26 +9,14 @@ export default function $domFadeTo(sel, o, t) {
         throw new Error('api-o');
     }
     t = parseInt(t);
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
-    if (Array.isArray(els)) {
-        for (var i = 0, len = els.length; i < len; i++) {
-            var el = els[i];
-            if (el) {
-                fadeTo(el, o, t);
-            }
+    var els = $toArrayOfElements(sel);
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            fadeTo(el);
         }
     }
-    function fadeTo(el, o, t) {
+    function fadeTo(el) {
         el.style.transition = 'opacity ' + (isNaN(t) || t < 0 ? 250 : t) + 'ms';
         el.style.opacity = o.toString();
     }
