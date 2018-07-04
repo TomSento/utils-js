0b74fe400b17b8ff33bfcd9a001e74a78b2fa7fb
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-04 10:22:48 +0200

    Updated: separated dom utils.

--- utils2/4_browser/dom/domFadeIn.js
+++ utils2/4_browser/dom/domFadeIn.js
@@ -1,28 +1,15 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import { $toArrayOfElements } from './_utils';
 
 export default function $domFadeIn(sel, t) {
     if (!sel) {
         throw new Error('api-sel');
     }
     t = parseInt(t);
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                fadeIn(el, t);
-            }
+    var els = $toArrayOfElements(sel);
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            fadeIn(el, t);
         }
     }
     function fadeIn(el, t) {
