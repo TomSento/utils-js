8a8107cfd839dc5ae15a277996714690b3495f56
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-05 19:45:09 +0200

    Updated: correction $domHTML().

--- utils2/4_browser/dom/domHTML.js
+++ utils2/4_browser/dom/domHTML.js
@@ -1,55 +1,34 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
+import $selectingOne from './internal/selectingOne';
 
 export default function $domHTML(sel, v) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    if (v && typeof(v) !== 'string') {
-        return new Error('api-v');
+    if (v !== undefined && typeof(v) !== 'string') {
+        throw new Error('api-v');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
+    var els = $toArrayOfElements(sel);
     var arr = [];
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (!el) {
-                continue;
-            }
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
             if (v === undefined) {
                 arr.push(getHtml(el));
             }
             else {
-                setHtml(el, v);
+                setHtml(el);
             }
         }
     }
-    return selectingOne(sel) ? arr[0] : arr;
+    if (v === undefined) {
+        return $selectingOne(sel) ? arr[0] : arr;
+    }
     function getHtml(el) {
         return el.innerHTML || null;
     }
-    function setHtml(el, v) {
+    function setHtml(el) {
         el.innerHTML = v;
-    }
-    function selectingOne(sel) {
-        if ($domIsEl(sel)) {
-            return true;
-        }
-        else if (typeof(sel) === 'string') {
-            var parts = sel.split(/\s+/);
-            return (parts && parts.length == 1 && parts[0][0] == '#');
-        }
-        return false;
     }
 }
 window.$domHTML = $domHTML;
