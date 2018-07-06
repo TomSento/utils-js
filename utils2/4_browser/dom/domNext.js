53159789fdcd2e56b812e6a79e766a39a8b5b71c
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-06 12:47:43 +0200

    Updated: correction $domNext().

--- utils2/4_browser/dom/domNext.js
+++ utils2/4_browser/dom/domNext.js
@@ -1,59 +1,38 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
+import $selectingOne from './internal/selectingOne';
 import $domMatches from './domMatches';
 
 export default function $domNext(sel, nsel) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
+    if (nsel !== undefined && typeof(nsel) !== 'string') {
+        throw new Error('api-nsel');
     }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
+    var els = $toArrayOfElements(sel);
     var arr = [];
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                arr.push(getNext(el, nsel));
-            }
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            arr.push(getNext(el));
         }
     }
-    return selectingOne(sel) ? arr[0] : arr;
-    function getNext(el, nsel) {
+    return $selectingOne(sel) ? arr[0] : arr;
+    function getNext(el) {
         var els = (el.parentNode && el.parentNode.children) ? el.parentNode.children : [];
         var brk = false;
-        for (var i = 0; i < els.length; i++) {
+        for (var i = 0, l = els.length; i < l; i++) {
             var sib = els[i];
+            if (sib && brk) {
+                if (nsel === undefined || $domMatches(sib, nsel)) {
+                    return sib;
+                }
+            }
             if (sib === el) {
                 brk = true;
-                continue;
-            }
-            if (brk && sib && ($domMatches(sib, nsel) || !nsel)) {
-                return sib;
-            }
-            else {
-                continue;
             }
         }
         return null;
-    }
-    function selectingOne(sel) {
-        if ($domIsEl(sel)) {
-            return true;
-        }
-        else if (typeof(sel) === 'string') {
-            var parts = sel.split(/\s+/);
-            return (parts && parts.length == 1 && parts[0][0] == '#');
-        }
-        return false;
     }
 }
 window.$domNext = $domNext;
