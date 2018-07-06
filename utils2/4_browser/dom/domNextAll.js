36d0fe7510a8b8691b89be3fc31b52ab993e90a9
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-06 16:45:47 +0200

    Updated: correction $domNextAll().

--- utils2/4_browser/dom/domNextAll.js
+++ utils2/4_browser/dom/domNextAll.js
@@ -1,55 +1,33 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
+import $selectingOne from './internal/selectingOne';
 
 export default function $domNextAll(sel) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
+    var els = $toArrayOfElements(sel);
     var arr = [];
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                arr.push(getNextAll(el));
-            }
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            arr.push(getNextAll(el));
         }
     }
-    return selectingOne(sel) ? arr[0] : arr;
+    return $selectingOne(sel) ? arr[0] : arr;
     function getNextAll(el) {
         var els = (el.parentNode && el.parentNode.children) ? el.parentNode.children : [];
         var arr = [];
         var brk = false;
-        for (var i = 0; i < els.length; i++) {
-            var ch = els[i];
-            if (brk) {
-                arr.push(ch);
+        for (var i = 0, l = els.length; i < l; i++) {
+            var sib = els[i];
+            if (sib && brk) {
+                arr.push(sib);
             }
-            if (ch === el) {
+            if (sib === el) {
                 brk = true;
             }
         }
         return arr;
-    }
-    function selectingOne(sel) {
-        if ($domIsEl(sel)) {
-            return true;
-        }
-        else if (typeof(sel) === 'string') {
-            var parts = sel.split(/\s+/);
-            return (parts && parts.length == 1 && parts[0][0] == '#');
-        }
-        return false;
     }
 }
 window.$domNextAll = $domNextAll;
