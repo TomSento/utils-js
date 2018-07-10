43f0874beec2adfebaee79adac76fa3e82192dd1
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-10 13:00:41 +0200

    Updated: correction $domRemove().

--- utils2/4_browser/dom/domRemove.js
+++ utils2/4_browser/dom/domRemove.js
@@ -1,27 +1,14 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
 
 export default function $domRemove(sel) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                domRemove(el);
-            }
+    var els = $toArrayOfElements(sel);
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            domRemove(el);
         }
     }
     function domRemove(el) {
