e65275d5ff985191a50feab92327270e8f69498d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-10 14:22:15 +0200

    Updated: correction $domRemoveClass().

--- utils2/4_browser/dom/domRemoveClass.js
+++ utils2/4_browser/dom/domRemoveClass.js
@@ -1,5 +1,4 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
 
 export default function $domRemoveClass(sel, v) {
     if (!sel) {
@@ -8,38 +7,30 @@ export default function $domRemoveClass(sel, v) {
     if (!v || typeof(v) !== 'string') {
         throw new Error('api-v');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
-    if (Array.isArray(els)) {
-        var len = els.length;
-        for (var i = 0; i < len; i++) {
-            var el = els[i];
-            if (!el) {
-                continue;
-            }
-            var clas = v.split(/\s+/);
-            var lenlen = clas.length;
-            for (var j = 0; j < lenlen; j++) {
-                var cla = clas[j];
-                removeClass(el, cla);
-            }
+    var els = $toArrayOfElements(sel);
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            removeClass(el);
         }
     }
-    function removeClass(el, v) {
-        if (el.classList) {
+    function removeClass(el) { // --------------------------------------------> REMOVES 0..N CLASSES
+        if (el.classList && typeof(el.classList.remove) === 'function') {
             el.classList.remove(v);
         }
         else {
-            el.className = el.className.replace(new RegExp('(^|\\b)' + v.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+            if (el.className && typeof(el.className) === 'string') {
+                var curr = el.className.split(/\s+/);
+                var rm = v.split(/\s+/);
+                var arr = [];
+                for (var i = 0, l = curr.length; i < l; i++) {
+                    var cls = curr[i];
+                    if (cls && rm.indexOf(cls) === -1) {
+                        arr.push(cls);
+                    }
+                }
+                el.className = arr.join(' ');
+            }
         }
     }
 }
