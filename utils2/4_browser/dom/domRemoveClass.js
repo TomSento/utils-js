35a3e51100fdf059a856956e47a974de009f2cc6
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-10 15:01:09 +0200

    Revert "Updated: correction $domRemoveClass()."
    This reverts commit f12ab2c7a499b6bc3d955ed28cd3865748ef7a3f.

--- utils2/4_browser/dom/domRemoveClass.js
+++ utils2/4_browser/dom/domRemoveClass.js
@@ -37,7 +37,7 @@ export default function $domRemoveClass(sel, rm) {
                         arr.push(cls);
                     }
                 }
-                el.className = arr.length > 0 ? arr.join(' ') : '';
+                el.className = arr.join(' ');
             }
         }
     }
