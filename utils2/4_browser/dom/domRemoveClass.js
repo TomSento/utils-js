0c56b47448934b2a44457d3595be5f1028f92dc6
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-10 14:45:45 +0200

    Updated: correction $domRemoveClass().

--- utils2/4_browser/dom/domRemoveClass.js
+++ utils2/4_browser/dom/domRemoveClass.js
@@ -1,12 +1,13 @@
 import $toArrayOfElements from './internal/toArrayOfElements';
 
-export default function $domRemoveClass(sel, v) {
+export default function $domRemoveClass(sel, rm) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    if (!v || typeof(v) !== 'string') {
+    if (!rm || typeof(rm) !== 'string') {
         throw new Error('api-v');
     }
+    rm = rm.split(/\s+/);
     var els = $toArrayOfElements(sel);
     for (var i = 0, l = els.length; i < l; i++) {
         var el = els[i];
@@ -15,16 +16,23 @@ export default function $domRemoveClass(sel, v) {
         }
     }
     function removeClass(el) { // --------------------------------------------> REMOVES 0..N CLASSES
+        var i;
+        var l;
+        var cls;
         if (el.classList && typeof(el.classList.remove) === 'function') {
-            el.classList.remove(v);
+            for (i = 0, l = rm.length; i < l; i++) {
+                cls = rm[i];
+                if (cls) {
+                    el.classList.remove(cls);
+                }
+            }
         }
         else {
             if (el.className && typeof(el.className) === 'string') {
                 var curr = el.className.split(/\s+/);
-                var rm = v.split(/\s+/);
                 var arr = [];
-                for (var i = 0, l = curr.length; i < l; i++) {
-                    var cls = curr[i];
+                for (i = 0, l = curr.length; i < l; i++) {
+                    cls = curr[i];
                     if (cls && rm.indexOf(cls) === -1) {
                         arr.push(cls);
                     }
