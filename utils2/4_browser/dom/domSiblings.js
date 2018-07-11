cbf75a2bd3eb7528a51b6b69ec507067a248c1fc
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-11 12:26:45 +0200

    Updated: correction $domSiblings().

--- utils2/4_browser/dom/domSiblings.js
+++ utils2/4_browser/dom/domSiblings.js
@@ -1,51 +1,33 @@
-import $domIsEl from './domIsEl';
-import $domFind from './domFind';
+import $toArrayOfElements from './internal/toArrayOfElements';
+import $arrUnique from '../../1_primitives/array/arrUnique';
+import $domMatches from './domMatches';
 
 export default function $domSiblings(sel) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    var els = null;
-    if (Array.isArray(sel)) {
-        els = sel;
-    }
-    else if ($domIsEl(sel)) {
-        els = [sel];
-    }
-    else {
-        els = $domFind(sel);
-        els = Array.isArray(els) ? els : [els];
-    }
+    var els = $toArrayOfElements(sel);
     var arr = [];
-    if (Array.isArray(els)) {
-        for (var i = 0; i < els.length; i++) {
-            var el = els[i];
-            if (el) {
-                arr.push(getSiblings(el));
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el) {
+            var sibs = getSiblings(el);
+            if (Array.isArray(sibs) && sibs.length > 0) {
+                arr = arr.concat(sibs);
             }
         }
     }
-    return selectingOne(sel) ? arr[0] : arr;
+    return $arrUnique(arr);
     function getSiblings(el) {
         var els = (el.parentNode && el.parentNode.children) ? el.parentNode.children : [];
         var arr = [];
-        for (var i = 0; i < els.length; i++) {
-            var ch = els[i];
-            if (ch !== el) {
-                arr.push(ch);
+        for (var i = 0, l = els.length; i < l; i++) {
+            var sib = els[i];
+            if (sib && !$domMatches(sib, sel)) {
+                arr.push(sib);
             }
         }
         return arr;
-    }
-    function selectingOne(sel) {
-        if ($domIsEl(sel)) {
-            return true;
-        }
-        else if (typeof(sel) === 'string') {
-            var parts = sel.split(/\s+/);
-            return (parts && parts.length == 1 && parts[0][0] == '#');
-        }
-        return false;
     }
 }
 window.$domSiblings = $domSiblings;
