50a58780a97471a11ff075cbb1c5edefd47f43f0
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-17 15:31:02 +0200

    Update: improved $domMatches() api validation.

--- utils2/4_browser/dom/domMatches.js
+++ utils2/4_browser/dom/domMatches.js
@@ -1,8 +1,16 @@
+import $domIsEl from './domIsEl';
+
 export default function $domMatches(el, sel) { // https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
-    if (typeof(Element.prototype.matches) === 'function') {
+    if (!el || !$domIsEl(el)) {
+        throw new Error('api-el');
+    }
+    if (!sel || typeof(sel) !== 'string') {
+        throw new Error('api-sel');
+    }
+    if (typeof(el.matches) === 'function') {
         return el.matches(sel);
     }
-    else if (typeof(Element.prototype.msMatchesSelector) === 'function') { // IE9+
+    else if (typeof(el.msMatchesSelector) === 'function') { // IE9+
         return el.msMatchesSelector(sel);
     }
     else {
