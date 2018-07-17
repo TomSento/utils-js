ca00ba8a28396e4af34798ba29d5c96fcae4ddf2
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-17 13:49:02 +0200

    Update: improved $domFadeTo() api validation.

--- utils2/4_browser/dom/domFadeTo.js
+++ utils2/4_browser/dom/domFadeTo.js
@@ -4,11 +4,12 @@ export default function $domFadeTo(sel, o, t) {
     if (!sel) {
         throw new Error('api-sel');
     }
-    o = parseFloat(o);
-    if (isNaN(o) || o < 0 || o > 1) {
+    if (typeof(o) !== 'number' || o < 0 || o > 1) {
         throw new Error('api-o');
     }
-    t = parseInt(t);
+    if (t !== undefined && (typeof(t) !== 'number' || t < 0)) {
+        throw new Error('api-t');
+    }
     var els = $toArrayOfElements(sel);
     for (var i = 0, l = els.length; i < l; i++) {
         var el = els[i];
@@ -17,8 +18,8 @@ export default function $domFadeTo(sel, o, t) {
         }
     }
     function fadeTo(el) {
-        el.style.transition = 'opacity ' + (isNaN(t) || t < 0 ? 250 : t) + 'ms';
-        el.style.opacity = o.toString();
+        el.style.transition = 'opacity ' + (t === undefined ? 250 : t) + 'ms';
+        el.style.opacity = '' + o;
     }
 }
 window.$domFadeTo = $domFadeTo;
