df1c7141784d1ab36a43df6fec01c049177fd805
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-18 14:16:09 +0200

    Added: node native modules used by "Controller1" to build config.

--- min.js
+++ min.js
@@ -23,7 +23,7 @@ async function dist(entry, format, out) {
 async function bundle(entry, format) {
     var bundle = await Rollup.rollup({
         input: entry,
-        external: format === 'cjs' ? ['fs', 'path', 'url', 'querystring'] : []
+        external: format === 'cjs' ? ['path', 'fs', 'url', 'querystring'] : []
     });
     return bundle.generate({ format });
 }
