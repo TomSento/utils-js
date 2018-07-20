f638ad600e1f859605f84dc7ec6e25309a9c0100
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-20 16:35:08 +0200

    Removed: "buffer" from rollup "external" config - Buffer is in Node.js global scope.

--- min.js
+++ min.js
@@ -23,7 +23,7 @@ async function dist(entry, format, out) {
 async function bundle(entry, format) {
     var bundle = await Rollup.rollup({
         input: entry,
-        external: format === 'cjs' ? ['path', 'fs', 'url', 'querystring', 'buffer'] : []
+        external: format === 'cjs' ? ['path', 'fs', 'url', 'querystring'] : []
     });
     return bundle.generate({ format });
 }
