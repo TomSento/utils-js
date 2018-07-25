bdf81a64a996e158674929384d7b722a1f3676bf
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-25 14:04:24 +0200

    Updated: requires naming - no logic change.

--- min.js
+++ min.js
@@ -1,7 +1,7 @@
-var Path = require('path');
-var Fs = require('fs');
-var Rollup = require('rollup');
-var Uglify = require('uglify-js');
+var $path = require('path');
+var $fs = require('fs');
+var $rollup = require('rollup');
+var $uglify = require('uglify-js');
 
 dist('./src/utils.browser.js', 'iife', './dist/utils.browser.js');
 dist('./src/utils.node.js', 'cjs', './dist/utils.node.js');
@@ -10,18 +10,18 @@ async function dist(entry, format, out) {
     var result = await bundle(entry, format);
     var code = result.code || '';
     if (process.argv.indexOf('-m') >= 0) {
-        var min = Uglify.minify(code);
+        var min = $uglify.minify(code);
         if (min.error) {
             kill(min.error);
         }
         code = min.code;
     }
-    Fs.writeFileSync(Path.resolve(out), code);
+    $fs.writeFileSync($path.resolve(out), code);
     var size = (Buffer.byteLength(code, 'utf8') / 1000).toFixed(1);
     log(entry, out, size);
 }
 async function bundle(entry, format) {
-    var bundle = await Rollup.rollup({
+    var bundle = await $rollup.rollup({
         input: entry,
         external: format === 'cjs' ? ['path', 'fs', 'url', 'querystring', 'https', 'http', 'os'] : []
     });
