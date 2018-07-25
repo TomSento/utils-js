ed74e18d177e892c8c61539d8367abd1aa116487
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-25 13:42:19 +0200

    Removed: "utils" directory. Updated: renamed "utils2" directory to "utils" .

--- src/utils.node.js
+++ src/utils.node.js
@@ -1,8 +1,8 @@
 import './utils.common';
 
-import '../utils2/3_node/fs/ls';
-import '../utils2/3_node/fs/Watcher';
+import '../utils/3_node/fs/ls';
+import '../utils/3_node/fs/Watcher';
 
-import '../utils2/3_node/server/0_route1';
-import '../utils2/3_node/server/1_Controller1';
-import '../utils2/3_node/server/2_server';
+import '../utils/3_node/server/0_route1';
+import '../utils/3_node/server/1_Controller1';
+import '../utils/3_node/server/2_server';
