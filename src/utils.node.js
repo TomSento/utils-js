96d4d2d362dce01cb749906112ab91ca7d766155
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-07-31 13:04:50 +0200

    Added: response constants for sending "json", "html" and "text".

--- src/utils.node.js
+++ src/utils.node.js
@@ -6,3 +6,4 @@ import '../utils/3_node/fs/Watcher';
 import '../utils/3_node/server/0_route1';
 import '../utils/3_node/server/1_Controller1';
 import '../utils/3_node/server/2_server';
+import '../utils/3_node/server/3_constants';
---
+++ utils/3_node/server/3_constants.js
@@ -0,0 +1,9 @@
+global.$RES_JSON = {
+    'Content-Type': 'application/json'
+};
+global.$RES_HTML = {
+    'Content-Type': 'text/html'
+};
+global.$RES_TEXT = {
+    'Content-Type': 'text/plain'
+};
