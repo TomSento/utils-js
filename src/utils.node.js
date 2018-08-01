b5d907d99dd722b9a6d50c588f45df0417a3b95f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-01 13:14:39 +0200

    Updated: $destroyStream() return value to "undefined". Added: $destroyStream() to node build.

--- src/utils.node.js
+++ src/utils.node.js
@@ -7,3 +7,5 @@ import '../utils/3_node/server/0_route1';
 import '../utils/3_node/server/1_Controller1';
 import '../utils/3_node/server/2_server';
 import '../utils/3_node/server/3_constants';
+
+import '../utils/3_node/destroyStream';
--- utils/3_node/destroyStream.js
+++ utils/3_node/destroyStream.js
@@ -14,6 +14,5 @@ export default function $destroyStream(stream) {
             stream.destroy();
         }
     }
-    return stream;
 }
 global.$destroyStream = $destroyStream;
