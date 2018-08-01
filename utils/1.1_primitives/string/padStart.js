d345a154fe6e6fb3bea53eca3cbb70935025a63f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-01 20:30:55 +0200

    Added: String.prototype.padStart() - polyfill.

---
+++ utils/1.1_primitives/string/padStart.js
@@ -0,0 +1,18 @@
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function(targetLength, padString) {
+        targetLength >>= 0; // eslint-disable-line no-bitwise
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength -= this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength / padString.length);
+            }
+            return padString.slice(0, targetLength) + String(this);
+        }
+    };
+}
