fb12fa533517fd7f1881cab99889553c0a78d865
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-01 14:34:06 +0200

    Added: Array.prototype.$findIndex().

---
+++ utils/1.1_primitives/array/findIndex.js
@@ -0,0 +1,22 @@
+Array.prototype.$findIndex = function(fn, v) {
+    var isFN = typeof(fn) === 'function';
+    var isV = v !== undefined;
+    for (var i = 0, len = this.length; i < len; i++) {
+        if (isFN) {
+            if (fn(this[i], i)) {
+                return i;
+            }
+            continue;
+        }
+        if (isV) {
+            if (this[i] && this[i][fn] === v) {
+                return i;
+            }
+            continue;
+        }
+        if (this[i] === fn) {
+            return i;
+        }
+    }
+    return -1;
+};
