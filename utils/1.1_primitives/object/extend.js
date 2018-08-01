ccc9a9020930833e7631555101c0f9f19b7605ec
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-01 18:04:19 +0200

    Added: $global.$extend().

---
+++ utils/1.1_primitives/object/extend.js
@@ -0,0 +1,24 @@
+import $global from '../../global';
+import $clone from './clone';
+
+export default function $extend(objA, objB, rewrite) {
+    if (!objA || typeof(objA) !== 'object') {
+        throw new Error('api-objA');
+    }
+    if (!objB || typeof(objB) !== 'object') {
+        throw new Error('api-objB');
+    }
+    if (rewrite === undefined) {
+        rewrite = true;
+    }
+    var keys = Object.keys(objB);
+    var i = keys.length;
+    while (i--) {
+        var key = keys[i];
+        if (rewrite || objA[key] === undefined) {
+            objA[key] = $clone(objB[key]);
+        }
+    }
+    return objA;
+}
+$global.$extend = $extend;
