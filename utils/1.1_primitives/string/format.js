5685f0c870f4d5d7f9dcee04896f5d8f4b81f03e
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-01 20:12:45 +0200

    Added: String.prototype.$format().

---
+++ utils/1.1_primitives/string/format.js
@@ -0,0 +1,7 @@
+String.prototype.$format = function() {
+    var args = arguments;
+    return this.replace(/\{\d+\}/g, function(text) {
+        var value = args[+text.substring(1, text.length - 1)];
+        return value === null ? '' : value;
+    });
+};
