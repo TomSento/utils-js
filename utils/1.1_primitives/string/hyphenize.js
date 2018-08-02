3cf15cad5469fece01d01d620f43f26cbf1ba931
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-02 09:09:07 +0200

    Updated: added hyphenization to $slug(). Removed: String.prototype.$hyphenize().

--- utils/1.1_primitives/string/hyphenize.js
+++
@@ -1,5 +0,0 @@
-String.prototype.$hyphenize = function() {
-    return this.replace(/\B([A-Z])/g, function(match) {
-        return ('-' + match[0]);
-    }).toLowerCase();
-};
--- utils/1.1_primitives/string/slug.js
+++ utils/1.1_primitives/string/slug.js
@@ -1,6 +1,8 @@
+var EXP_HYPHENIZE = /\B[A-Z]+[a-z]/g;
+
 String.prototype.$slug = function(max) {
     max = max || 60;
-    var self = this.trim().toLowerCase().$removeDiacritics();
+    var self = hyphenize(this.trim().$removeDiacritics());
     var builder = '';
     var length = self.length;
     for (var i = 0; i < length; i++) {
@@ -21,4 +23,9 @@ String.prototype.$slug = function(max) {
     }
     var l = builder.length - 1;
     return builder[l] === '-' ? builder.substring(0, l) : builder;
+    function hyphenize(str) {
+        return str.replace(EXP_HYPHENIZE, function(m) {
+            return '-' + m;
+        }).toLowerCase();
+    }
 };
