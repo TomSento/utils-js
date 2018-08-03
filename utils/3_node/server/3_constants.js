73fde7675576f19e74fbe2e22f52ae857c703f64
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-03 18:49:51 +0200

    Removed: response constants.

--- utils/3_node/server/3_constants.js
+++
@@ -1,9 +0,0 @@
-global.$RES_JSON = {
-    'Content-Type': 'application/json'
-};
-global.$RES_HTML = {
-    'Content-Type': 'text/html'
-};
-global.$RES_TEXT = {
-    'Content-Type': 'text/plain'
-};
