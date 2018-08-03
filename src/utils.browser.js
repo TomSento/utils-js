93ec89f385bf9338c5d3266d56615a49c08b14da
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-03 11:56:08 +0200

    Added: HTMLCollection.$from().

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,9 +1,10 @@
 import './utils.common';
 
-import '../utils/4_browser/CustomEvent.$create';
-
 import '../utils/4_browser/router/0_route0';
 import '../utils/4_browser/router/1_Controller0';
 
 import '../utils/4_browser/ajax';
 import '../utils/4_browser/browserSupportsEvent';
+
+import '../utils/4_browser/CustomEvent.$create';
+import '../utils/4_browser/HTMLCollection.$from';
---
+++ utils/4_browser/HTMLCollection.$from.js
@@ -0,0 +1,12 @@
+HTMLCollection.$from = function(str) { // ------------------------------------> https://github.com/nefe/You-Dont-Need-jQuery#6.4
+    var context = document.implementation.createHTMLDocument();
+
+    // Set the base href for the created document so any parsed elements with URLs
+    // are based on the document's URL
+    var base = context.createElement('base');
+    base.href = document.location.href;
+    context.head.appendChild(base);
+
+    context.body.innerHTML = str;
+    return context.body.children;
+};
