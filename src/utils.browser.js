7eac27d38aefa69fbd24f1d4666da3b8515b106e
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-04 17:41:34 +0200

    Removed: old $dom* tests. Removed: $route0 and $Controller0.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,8 +1,5 @@
 import './utils.common';
 
-import '../utils/4_browser/router/0_route0';
-import '../utils/4_browser/router/1_Controller0';
-
 import '../utils/4_browser/ajax';
 import '../utils/4_browser/browserSupportsEvent';
 
