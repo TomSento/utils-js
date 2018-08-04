d6fd71914882b5b032be1a7c1b533f14acda4cae
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-04 18:30:20 +0200

    Renamed: ajax.js to xhr.js.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,7 +1,8 @@
 import './utils.common';
 
-import '../utils/4_browser/ajax';
 import '../utils/4_browser/browserSupportsEvent';
 
 import '../utils/4_browser/CustomEvent.$create';
 import '../utils/4_browser/HTMLCollection.$from';
+
+import '../utils/4_browser/xhr';
