b2b1a639d3fc3ea7b2bb49dad0952e12edc55ec7
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-05 14:34:27 +0200

    Added: global constant "d" as alias to "document".

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,8 +1,7 @@
 import './utils.common';
 
 import '../utils/4_browser/browserSupportsEvent';
-
 import '../utils/4_browser/CustomEvent.$create';
+import '../utils/4_browser/d';
 import '../utils/4_browser/HTMLCollection.$from';
-
 import '../utils/4_browser/xhr';
