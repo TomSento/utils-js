e6a893c3aa9c4ade81c624423fa746e22a687084
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-05 20:07:38 +0200

    Updated: filenames according new convention - node utils - part 2.

--- src/utils.node.js
+++ src/utils.node.js
@@ -1,10 +1,9 @@
 import './utils.common';
 
-import '../utils/3_node/fs/ls';
-import '../utils/3_node/fs/Watcher';
+import '../utils/3_node/fs/$ls';
+import '../utils/3_node/fs/$Watcher';
 
-import '../utils/3_node/server/0_route1';
-import '../utils/3_node/server/1_Controller1';
-import '../utils/3_node/server/2_server';
+import '../utils/3_node/server/$route';
+import '../utils/3_node/server/$server';
 
-import '../utils/3_node/destroyStream';
+import '../utils/3_node/$destroyStream';
