fab5acf34f4d1adf6da93aa90c95ae0a0ca92a18
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-06 19:07:40 +0200

    Updated: module system - node utils - part2.

--- src/utils.node.js
+++ src/utils.node.js
@@ -1,9 +1,9 @@
 import './utils.common';
 
-import '../utils/3_node/fs/$ls';
-import '../utils/3_node/fs/$Watcher';
+import '../utils/3_node/fs/ls';
+import '../utils/3_node/fs/Watcher';
 
-import '../utils/3_node/server/$route';
-import '../utils/3_node/server/$server';
+import '../utils/3_node/server/route';
+import '../utils/3_node/server/server';
 
-import '../utils/3_node/$destroyStream';
+import '../utils/3_node/destroyStream';
