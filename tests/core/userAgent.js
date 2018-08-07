0c4798f4c8b8ae624afef53efaa0d7c79aab91f0
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-07 16:19:29 +0200

    Updated: test - userAgent.

--- tests/core/userAgent.js
+++ tests/core/userAgent.js
@@ -6,47 +6,50 @@ var os = require('../files/userAgent_os.json');
 var devices = require('../files/userAgent_devices.json');
 var cpu = require('../files/userAgent_cpu.json');
 
-browsers.forEach(function(test) {
-    $test('(browser) ' + test.desc, function(assert) {
-        var v = $userAgent(test.ua).browserName;
-        assert.ok(v === test.expect.name, v, test.expect.name, '(browser/name) ' + test.desc);
-        v = $userAgent(test.ua).browserVersion;
-        assert.ok(v === test.expect.version, v, test.expect.version, '(browser/version) ' + test.desc);
+var test = $import('<test>');
+var userAgent = $import('<userAgent>');
+
+browsers.forEach(function(data) {
+    test('(browser) ' + data.desc, function(assert) {
+        var v = userAgent(data.ua).browserName;
+        assert.ok(v === data.expect.name, v, data.expect.name, '(browser/name) ' + data.desc);
+        v = userAgent(data.ua).browserVersion;
+        assert.ok(v === data.expect.version, v, data.expect.version, '(browser/version) ' + data.desc);
     });
 });
 
-engines.forEach(function(test) {
-    $test('(engine) ' + test.desc, function(assert) {
-        var v = $userAgent(test.ua).engineName;
-        assert.ok(v === test.expect.name, v, test.expect.name, '(engine/name) ' + test.desc);
-        v = $userAgent(test.ua).engineVersion;
-        assert.ok(v === test.expect.version, v, test.expect.version, '(engine/version) ' + test.desc);
+engines.forEach(function(data) {
+    test('(engine) ' + data.desc, function(assert) {
+        var v = userAgent(data.ua).engineName;
+        assert.ok(v === data.expect.name, v, data.expect.name, '(engine/name) ' + data.desc);
+        v = userAgent(data.ua).engineVersion;
+        assert.ok(v === data.expect.version, v, data.expect.version, '(engine/version) ' + data.desc);
     });
 });
 
-os.forEach(function(test) {
-    $test('(os) ' + test.desc, function(assert) {
-        var v = $userAgent(test.ua).osName;
-        assert.ok(v === test.expect.name, v, test.expect.name, '(os/name) ' + test.desc);
-        v = $userAgent(test.ua).osVersion;
-        assert.ok(v === test.expect.version, v, test.expect.version, '(os/version) ' + test.desc);
+os.forEach(function(data) {
+    test('(os) ' + data.desc, function(assert) {
+        var v = userAgent(data.ua).osName;
+        assert.ok(v === data.expect.name, v, data.expect.name, '(os/name) ' + data.desc);
+        v = userAgent(data.ua).osVersion;
+        assert.ok(v === data.expect.version, v, data.expect.version, '(os/version) ' + data.desc);
     });
 });
 
-devices.forEach(function(test) {
-    $test('(device) ' + test.desc, function(assert) {
-        var v = $userAgent(test.ua).deviceVendor;
-        assert.ok(v === test.expect.vendor, v, test.expect.vendor, '(device/vendor) ' + test.desc);
-        v = $userAgent(test.ua).deviceModel;
-        assert.ok(v === test.expect.model, v, test.expect.model, '(device/model) ' + test.desc);
-        v = $userAgent(test.ua).deviceType;
-        assert.ok(v === test.expect.type, v, test.expect.type, '(device/type) ' + test.desc);
+devices.forEach(function(data) {
+    test('(device) ' + data.desc, function(assert) {
+        var v = userAgent(data.ua).deviceVendor;
+        assert.ok(v === data.expect.vendor, v, data.expect.vendor, '(device/vendor) ' + data.desc);
+        v = userAgent(data.ua).deviceModel;
+        assert.ok(v === data.expect.model, v, data.expect.model, '(device/model) ' + data.desc);
+        v = userAgent(data.ua).deviceType;
+        assert.ok(v === data.expect.type, v, data.expect.type, '(device/type) ' + data.desc);
     });
 });
 
-cpu.forEach(function(test) {
-    $test('(cpu) ' + test.desc, function(assert) {
-        var v = $userAgent(test.ua).cpu;
-        assert.ok(v === test.expect.architecture, v, test.expect.architecture, '(cpu) ' + test.desc);
+cpu.forEach(function(data) {
+    test('(cpu) ' + data.desc, function(assert) {
+        var v = userAgent(data.ua).cpu;
+        assert.ok(v === data.expect.architecture, v, data.expect.architecture, '(cpu) ' + data.desc);
     });
 });
