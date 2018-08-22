a3c763284a2679e7aef51f3ba00eaeb26b7f3dbe
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-22 13:13:45 +0200

    Added: browser utility - Cookie.get() - part3 - added "undefined" return value.

--- utils/4_browser/Cookie.get.js
+++ utils/4_browser/Cookie.get.js
@@ -1,6 +1,10 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Example_2_Get_a_sample_cookie_named_test2
 var Cookie = {
     get: function(k) {
+        var i = document.cookie.lastIndexOf('; ' + k + '=');
+        if (i === -1 && !document.cookie.startsWith(k + '=')) {
+            return undefined;
+        }
         var exp = new RegExp('(?:(?:^|.*;\\s*)' + k + '\\s*=\\s*([^;]*).*$)|^.*$');
         return document.cookie.replace(exp, '$1');
     }
