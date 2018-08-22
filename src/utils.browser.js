e3e7f6e1bf93f11dbb1642f593bc195ff74ad45f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-22 12:35:02 +0200

    Added: browser utility - Cookie.get() - part2.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,6 +1,7 @@
 import './utils.common';
 
 import '../utils/4_browser/browserSupportsEvent';
+import '../utils/4_browser/Cookie.get';
 import '../utils/4_browser/CustomEvent.create';
 import '../utils/4_browser/d';
 import '../utils/4_browser/Element.prototype.matches';
