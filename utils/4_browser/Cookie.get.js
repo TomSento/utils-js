94bb95b39774dffab936bc9698eb295c1322c297
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-23 09:26:47 +0200

    Fixed: Cookie.get() - added argument validation.

--- utils/4_browser/Cookie.get.js
+++ utils/4_browser/Cookie.get.js
@@ -1,6 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Example_2_Get_a_sample_cookie_named_test2
 var Cookie = {
     get: function(k) {
+        if (!k || typeof(k) !== 'string') {
+            throw new Error('api-k');
+        }
         var i = document.cookie.lastIndexOf('; ' + k + '=');
         if (i === -1 && !document.cookie.startsWith(k + '=')) {
             return undefined;
