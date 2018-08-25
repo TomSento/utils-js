a14902338227f55c63c21a7f434fa241e9aeca8c
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-25 14:25:16 +0200

    Added: new request handler - initial commit.

---
+++ utils/3_node/server/handleRequest.js
@@ -0,0 +1,37 @@
+import * as $path from 'path';
+import * as $fs from 'fs';
+import * as $url from 'url';
+import * as $querystring from 'querystring';
+import $malloc from '../../0_internal/malloc';
+import $MultipartParser from './internal/MultipartParser';
+import $destroyStream from '../destroyStream';
+
+var STATIC_ACCEPTS = [
+    '.txt', '.md',
+    '.html', '.xml', '.json',
+    '.woff', '.woff2', '.otf', '.ttf', '.eot',
+    '.js', '.css',
+    '.jpg', '.png', '.gif', '.svg', '.ico',
+    '.mp4', '.mp3', '.swf',
+    '.pdf', '.docx', '.xlsx', '.doc', '.xls',
+    '.zip', '.rar'
+];
+var EXP_ONLY_SLASHES = /^\/{2,}$/;
+var RES_FN_CALLS_BLACKLIST = [ // --------------------------------------------> EXCEPT end()
+    'addTrailers',
+    'removeHeader',
+    'setHeader',
+    'setTimeout',
+    'write',
+    'writeContinue',
+    'writeHead',
+    'writeProcessing'
+];
+var MAX_URL_LEN = 2000;
+var MAX_URL_QUERY_LEN = 1000;
+var FILE_INDEX = 0;
+var CONCAT = [null, null];
+
+export default function handleRequest(req, res, routeError) {
+
+}
