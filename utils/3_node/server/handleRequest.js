d16cf4f529da67472ddfb39637f6ef6cfb5153e2
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-27 14:58:47 +0200

    Added: new request handler - prepareRequestMULTIPART - part1 - parseMultipartHeader.

--- utils/3_node/server/handleRequest.js
+++ utils/3_node/server/handleRequest.js
@@ -238,3 +238,25 @@ function prepareRequestJSON(req, res, routeError, route, next) {
         }
     });
 }
+
+function parseMultipartHeader(header) {
+    var tmp = '';
+    var search = ' name="';
+    var len = search.length;
+    var beg = header.indexOf(search);
+    if (beg >= 0) {
+        tmp = header.slice(beg + len, header.indexOf('"', beg + len));
+    }
+    var name = tmp ? tmp : ('undefined_' + Math.floor(Math.random() * 100000)); // HTML INPUT NAME
+    tmp = '';
+    search = ' filename="';
+    len = search.length;
+    beg = header.indexOf(search);
+    if (beg >= 0) {
+        tmp = header.slice(beg + len, header.indexOf('"', beg + len));
+    }
+    return {
+        name: name,
+        filename: tmp || null
+    };
+}
