c21650dbc01b1fadf2e1dc2b5ab1383011789ef7
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-30 15:02:11 +0200

    Added: extension to content type map.

---
+++ utils/ext2ct.js
@@ -0,0 +1,36 @@
+export default {
+    '.txt': 'text/plain',
+    '.md': 'text/markdown',
+
+    '.html': 'text/html',
+    '.xml': 'application/xml',
+    '.json': 'application/json',
+
+    '.woff': 'font/woff',
+    '.woff2': 'font/woff2',
+    '.otf': 'font/otf',
+    '.ttf': 'font/ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+
+    '.jpg': 'image/jpeg',
+    '.png': 'image/png',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon',
+
+    '.mp4': 'video/mp4',
+    '.mp3': 'audio/mp3',
+    '.swf': 'application/x-shockwave-flash',
+
+    '.pdf': 'application/pdf',
+    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    '.doc': 'application/msword',
+    '.xls': 'application/vnd.ms-excel',
+
+    '.zip': 'application/zip',
+    '.rar': 'application/x-rar-compressed'
+};
