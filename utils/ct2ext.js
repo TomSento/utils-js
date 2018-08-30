15004653816ac88a5d651d64f9d49d7105559b9d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-30 14:52:21 +0200

    Updated: "extension()" to use external "ct2ext" data object.

---
+++ utils/ct2ext.js
@@ -0,0 +1,37 @@
+export default {
+    'text/plain': '.txt',
+    'text/markdown': '.md',
+
+    'text/html': '.html',
+    'application/xml': '.xml',
+    'text/xml': '.xml',
+    'application/json': '.json',
+
+    'font/woff': '.woff',
+    'font/woff2': '.woff2',
+    'font/otf': '.otf',
+    'font/ttf': '.ttf',
+    'application/vnd.ms-fontobject': '.eot',
+
+    'application/javascript': '.js',
+    'text/css': '.css',
+
+    'image/jpeg': '.jpg',
+    'image/png': '.png',
+    'image/gif': '.gif',
+    'image/svg+xml': '.svg',
+    'image/x-icon': '.ico',
+
+    'video/mp4': '.mp4',
+    'audio/mp3': '.mp3', // NO "audio/mpeg" SUPPORT
+    'application/x-shockwave-flash': '.swf',
+
+    'application/pdf': '.pdf',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': '.xlsx',
+    'application/msword': '.doc',
+    'application/vnd.ms-excel': '.xls',
+
+    'application/zip': '.zip',
+    'application/x-rar-compressed': '.rar'
+};
