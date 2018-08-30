e992e1214ed5bfdf17feaddd327eca80c723cdd1
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-30 08:39:22 +0200

    Remove: destroyStream() - part1.

--- src/utils.node.js
+++ src/utils.node.js
@@ -6,5 +6,3 @@ import '../utils/3_node/fs/Watcher';
 import '../utils/3_node/server/request';
 import '../utils/3_node/server/route';
 import '../utils/3_node/server/Server';
-
-import '../utils/3_node/destroyStream';
