0fd8a7d82f128664e18c84f3b9bcb61337594ab5
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-08-31 12:08:11 +0200

    Fixed: serve static file path traversal vulnerability.

--- utils/3_node/server/handleRequest.js
+++ utils/3_node/server/handleRequest.js
@@ -50,7 +50,11 @@ function prepareRoute(req, res, routeError, next) {
     if (!ext || !$ext2ct.hasOwnProperty(ext)) {
         return routeError(req, res, 404, null);
     }
-    var filepath = $path.resolve('./public' + pathname);
+    var basepath = process.cwd() + $path.sep + 'public';
+    var filepath = $path.normalize(basepath + pathname);
+    if (filepath.indexOf(basepath + $path.sep) !== 0) { // -------------------> PATH TRAVERSAL VULNERABILITY CHECK
+        return routeError(req, res, 404, null);
+    }
     $fs.stat(filepath, function(err) {
         if (err) {
             return routeError(req, res, err.code === 'ENOENT' ? 404 : 500, null);
