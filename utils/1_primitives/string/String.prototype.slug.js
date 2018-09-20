68edb538ad0bdedffabdcb537f83adf9b0b6f6de
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-09-20 15:05:02 +0200

    Removed: slug hyphenization.

--- utils/1_primitives/string/String.prototype.slug.js
+++ utils/1_primitives/string/String.prototype.slug.js
@@ -1,8 +1,6 @@
-var EXP_HYPHENIZE = /\B[A-Z]+[a-z]/g;
-
 String.prototype.slug = function(max) {
     max = max || 60;
-    var self = hyphenize(this.trim().removeDiacritics());
+    var self = this.trim().removeDiacritics();
     var builder = '';
     var length = self.length;
     for (var i = 0; i < length; i++) {
@@ -23,9 +21,4 @@ String.prototype.slug = function(max) {
     }
     var l = builder.length - 1;
     return builder[l] === '-' ? builder.substring(0, l) : builder;
-    function hyphenize(str) {
-        return str.replace(EXP_HYPHENIZE, function(m) {
-            return '-' + m;
-        }).toLowerCase();
-    }
 };
