a5f42eb7081f0a4ad7960a28088f8d948aa3b409
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-09-23 08:16:05 +0200

    Updated: replaced Cookie.get() implementation +performance, +simplicity.

--- utils/4_browser/Cookie.get.js
+++ utils/4_browser/Cookie.get.js
@@ -1,27 +1,18 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Example_2_Get_a_sample_cookie_named_test2
 var Cookie = {
     get: function(k) {
-        if (!k || typeof(k) !== 'string') {
-            throw new Error('api-k');
-        }
         var str = document.cookie;
-        var i = 0;
-        while (i < str.length) {
-            var j = str.indexOf('=', i + 1);
-            if (j === -1) {
-                return undefined;
-            }
-            var end = str.indexOf(';', j + 1);
-            if (end === -1) {
-                end = str.length;
-            }
-            var key = str.slice(i, j).trim();
-            if (key === k) {
-                return str.slice(j + 1, end);
-            }
-            i = end + 1;
+        var i = str.indexOf(k + '=');
+        if (i === -1) {
+            return undefined;
         }
-        return undefined;
+        var j;
+        if (i > 0 && str[i - 1] !== ' ') {
+            return undefined;
+        }
+        j = i + k.length + 1;
+        var end = str.indexOf(';', j);
+        return str.slice(j, end === -1 ? undefined : end);
     }
 };
 $export('<Cookie>', Cookie);
