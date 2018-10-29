bf5078326999682842559b00394ed142d934222c
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-10-29 17:53:40 +0100

    Updated: simplified schema.

--- utils/2_core/Schema.js
+++ utils/2_core/Schema.js
@@ -1,18 +1,8 @@
-function Schema(obj) {
-    if (Object.prototype.toString.call(obj) !== '[object Object]') {
-        throw new Error('api-obj');
+function Schema(rules) {
+    if (Object.prototype.toString.call(rules) !== '[object Object]') {
+        throw new Error('api-rules');
     }
-    this.rule = {};
-    for (var k in obj) {
-        if (obj.hasOwnProperty(k)) {
-            var tmp = obj[k][1];
-            this.rule[k] = {
-                msg: obj[k][0],
-                prepare: tmp.prepare,
-                validate: tmp.validate
-            };
-        }
-    }
+    this.rules = rules;
 }
 Schema.prototype = {
     prepareValidate: function(o) {
@@ -20,16 +10,16 @@ Schema.prototype = {
             throw new Error('api-o');
         }
         var err = {};
-        for (var k in this.rule) {
-            if (this.rule.hasOwnProperty(k)) {
-                var rule = this.rule[k];
+        for (var k in this.rules) {
+            if (this.rules.hasOwnProperty(k)) {
+                var rule = this.rules[k];
                 var v = o[k];
                 if (typeof(rule.prepare) === 'function') {
                     v = rule.prepare(v, o);
                     o[k] = v;
                 }
                 if (rule.validate && !rule.validate(v, o)) {
-                    err[k] = rule.msg;
+                    err[k] = true;
                 }
             }
         }
@@ -40,22 +30,12 @@ Schema.prototype = {
             throw new Error('api-o');
         }
         var tmp = {};
-        for (var k in this.rule) {
-            if (this.rule.hasOwnProperty(k)) {
+        for (var k in this.rules) {
+            if (this.rules.hasOwnProperty(k)) {
                 tmp[k] = o[k];
             }
         }
         return tmp;
-    },
-    error: function(k) {
-        if (!k || typeof(k) !== 'string') {
-            throw new Error('api-k');
-        }
-        var rule = this.rule[k];
-        if (!rule) {
-            throw new Error('Property "' + k + '" not found.');
-        }
-        return rule.msg;
     }
 };
 $export('<Schema>', Schema);
