e0b889071780da6d6f45bd5d2ce6bdb8d9e64a98
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 13:25:03 +0100

    Deprecate: server.

--- src/utils.node.js
+++ src/utils.node.js
@@ -6,4 +6,3 @@ import '../utils/3_node/fs/Watcher';
 export { default as handleRequest } from '../utils/3_node/server/handleRequest';
 export { default as request } from '../utils/3_node/server/request';
 export { default as route } from '../utils/3_node/server/route';
-import '../utils/3_node/server/Server';
