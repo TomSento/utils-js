3f74245b4d12ba78389db2e8029c39c143138154
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 17:16:53 +0100

    Fixed: import.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,6 +1,6 @@
 import './utils.common';
 
-import '../utils/4_browser/Cor.Cookie.get';
+import '../utils/4_browser/Cookie.get';
 import '../utils/4_browser/CustomEvent.create';
 import '../utils/4_browser/Element.prototype.matches';
 import '../utils/4_browser/Element.prototype.removeChild';
