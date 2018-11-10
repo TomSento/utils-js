49588dff7016acf700c776c3807244627a7b45ec
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 17:34:53 +0100

    Updated: server utils - added Cor prefix to filenames.

--- src/utils.node.js
+++ src/utils.node.js
@@ -3,6 +3,6 @@ export * from './utils.common';
 export { default as ls } from '../utils/3_node/fs/Cor.ls';
 export { default as Watcher } from '../utils/3_node/fs/Cor.Watcher';
 
-export { default as handleRequest } from '../utils/3_node/server/handleRequest';
+export { default as handleRequest } from '../utils/3_node/server/Cor.handleRequest';
 export { default as request } from '../utils/3_node/server/request';
 export { default as route } from '../utils/3_node/server/route';
