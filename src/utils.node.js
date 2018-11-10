29e6f1b4bccefe848072b10332ee481de5e02cb0
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 17:32:41 +0100

    Updated: fs utils - added Cor prefix to filenames.

--- src/utils.node.js
+++ src/utils.node.js
@@ -1,7 +1,7 @@
 export * from './utils.common';
 
-export { default as ls } from '../utils/3_node/fs/ls';
-export { default as Watcher } from '../utils/3_node/fs/Watcher';
+export { default as ls } from '../utils/3_node/fs/Cor.ls';
+export { default as Watcher } from '../utils/3_node/fs/Cor.Watcher';
 
 export { default as handleRequest } from '../utils/3_node/server/handleRequest';
 export { default as request } from '../utils/3_node/server/request';
