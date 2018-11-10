8bef48bfcedb8228678d7592872924a87fef83ae
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 14:19:31 +0100

    Removed: browserSupportsEvent.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,6 +1,5 @@
 import './utils.common';
 
-import '../utils/4_browser/browserSupportsEvent';
 import '../utils/4_browser/Cor.Cookie.get';
 import '../utils/4_browser/CustomEvent.create';
 import '../utils/4_browser/Element.prototype.matches';
