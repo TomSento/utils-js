bace35c1eec024ef5b15cab3351cde951578051d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 15:00:57 +0100

    Updated: access "uid2" in browser using "Cor.uid2".

--- utils/2_core/uid2.js
+++ utils/2_core/uid2.js
@@ -1,4 +1,7 @@
 function uid2() {
     return [Math.random().toString(36).slice(2, 8), Math.random().toString(36).slice(2, 8), Math.random().toString(36).slice(2, 8), Math.random().toString(36).slice(2, 8)].join('-');
 }
-$export('<uid2>', uid2);
+if (window) {
+    if (!window.Cor) window.Cor = {};
+    window.Cor.uid2 = uid2;
+}
