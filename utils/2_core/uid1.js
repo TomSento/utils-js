b9e9064c40ae6ae13f2f515f1c13ebf9d17e8768
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 17:07:54 +0100

    Updated: access "uid1" as "Cor.uid1" also in node.

--- utils/2_core/uid1.js
+++ utils/2_core/uid1.js
@@ -1,7 +1,7 @@
+import world from '../world';
+
 export default function uid1() {
     return ['' + new Date().getFullYear(), ('' + (new Date().getMonth() + 1)).padStart(2, '0'), ('' + (new Date().getDate())).padStart(2, '0'), ('' + (new Date().getHours())).padStart(2, '0'), ('' + (new Date().getMinutes())).padStart(2, '0'), Math.random().toString(36).slice(2, 8)].join('-');
 }
-if (typeof(window) === 'object') {
-    if (!window.Cor) window.Cor = {};
-    window.Cor.uid1 = uid1;
-}
+if (!world.Cor) world.Cor = {};
+world.Cor.uid1 = uid1;
