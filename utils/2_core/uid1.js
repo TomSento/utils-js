054b3ea5f4642639e05446d425db028bf265f6dc
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 14:57:52 +0100

    Updated: access "uid1" in browser using "Cor.uid1".

--- utils/2_core/uid1.js
+++ utils/2_core/uid1.js
@@ -1,4 +1,7 @@
 function uid1() {
     return ['' + new Date().getFullYear(), ('' + (new Date().getMonth() + 1)).padStart(2, '0'), ('' + (new Date().getDate())).padStart(2, '0'), ('' + (new Date().getHours())).padStart(2, '0'), ('' + (new Date().getMinutes())).padStart(2, '0'), Math.random().toString(36).slice(2, 8)].join('-');
 }
-$export('<uid1>', uid1);
+if (window) {
+    if (!window.Cor) window.Cor = {};
+    window.Cor.uid1 = uid1;
+}
