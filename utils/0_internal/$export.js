1f967db65df8de55b8f91ca5b90e62add8dd3b9e
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 14:20:58 +0100

    Removed: $export, $import.

--- utils/0_internal/$export.js
+++
@@ -1,8 +0,0 @@
-import $global from '../global';
-
-$global.$export = function(k, v) {
-    if (!$global.$exports) {
-        $global.$exports = {};
-    }
-    $global.$exports[k] = v;
-};
--- utils/0_internal/$import.js
+++
@@ -1,5 +0,0 @@
-import $global from '../global';
-
-$global.$import = function(k) {
-    return $global.$exports[k] || null;
-};
