c283128adcb2ca832613001d3eb2a2ac152d6729
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-10 16:59:42 +0100

    Updated: access "extension" as "Cor.extension" also in node.

--- utils/1_primitives/string/extension.js
+++ utils/1_primitives/string/extension.js
@@ -1,9 +1,8 @@
-import $ct2ext from '../../ct2ext';
+import world from '../../world';
+import ct2ext from '../../ct2ext';
 
 export default function extension(k) {
-    return $ct2ext[k] || null;
+    return ct2ext[k] || null;
 }
-if (typeof(window) === 'object') {
-    if (!window.Cor) window.Cor = {};
-    window.Cor.extension = extension;
-}
+if (!world.Cor) world.Cor = {};
+world.Cor.extension = extension;
