18e38ff2a5d946010308e47521da3f851dcbca8f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-14 11:29:06 +0100

    Added: regex to match regexes.

--- utils/3_node/Cor.minScript.js
+++ utils/3_node/Cor.minScript.js
@@ -1,3 +1,14 @@
+// - remove comments
+// - ensure that { or } is not inside string or inside regex;
+var openBracketIndexes = [];
+var blindRanges = []; // strings, regular expressions
+var EXP_MATCH_REGEXES = /[|&[(=]\s*(\/.+\/)/; // https://regex101.com/r/vCs702/1/
+
 export default function minScript(str) {
+    blindRanges = getBlindRanges();
     return str;
+}
+
+function getBlindRanges() {
+
 }
