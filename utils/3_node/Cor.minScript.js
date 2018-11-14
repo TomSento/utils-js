9c445bd740974c72250c7d375cbbedc7f43c549f
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-14 12:03:53 +0100

    Added: remove block comments.

--- utils/3_node/Cor.minScript.js
+++ utils/3_node/Cor.minScript.js
@@ -5,20 +5,24 @@ var blindRanges = []; // strings, regular expressions
 var EXP_MATCH_REGEXES = /[|&[(=]\s*(\/.+\/)/; // https://regex101.com/r/vCs702/1/
 
 export default function minScript(str) {
+    str = removeBlockComments(str);
     blindRanges = getBlindRanges();
     return str;
 }
 
 function removeBlockComments(str) {
-    var searchStart = '/**';
-    var searchEnd = '*/';
     var i;
-    var j;
+    var searchStart = '/**';
+    var j = 0;
+    var b = '';
+    var searchEnd = '*/';
     while (i !== -1) {
         i = str.indexOf(searchStart, j);
         if (i === -1) {
+            b += str.slice(j);
             continue;
         }
+        b += str.slice(j, i);
         j = str.indexOf(searchEnd, j + 2); // ————————————————————————————————— + 2 BECAUSE /**/ IS ALLOWED
         if (j === -1) {
             i = -1;
@@ -26,6 +30,7 @@ function removeBlockComments(str) {
         }
         j += 2;
     }
+    return b;
 }
 
 function getBlindRanges() {
