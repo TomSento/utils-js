d5455b48bb18c6238de801ec6bac303a762cb23d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-14 10:31:15 +0100

    Added: minScript - initial commit.

--- src/utils.node.js
+++ src/utils.node.js
@@ -6,3 +6,5 @@ export { default as Watcher } from '../utils/3_node/fs/Cor.Watcher';
 export { default as handleRequest } from '../utils/3_node/server/Cor.handleRequest';
 export { default as request } from '../utils/3_node/server/Cor.request';
 export { default as route } from '../utils/3_node/server/Cor.route';
+
+export { default as minScript } from '../utils/3_node/Cor.minScript';
---
+++ utils/3_node/Cor.minScript.js
@@ -0,0 +1,3 @@
+export default function minScript(str) {
+    return str;
+}
