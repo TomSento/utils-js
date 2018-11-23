ffc8beda37e54832ff6db2d4383affbfc232699d
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-11-23 11:16:32 +0100

    Remove: redundant regex loop condition.

--- utils/3_node/Cor.compileBrowserScript.js
+++ utils/3_node/Cor.compileBrowserScript.js
@@ -89,15 +89,13 @@ function getSingleQuoteStringRanges(str) {
     var m;
     var ranges = [];
     while (m = EXP_MATCH_SINGLE_QUOTE_STRING.exec(str)) {
-        if (Array.isArray(m) && m.length > 0) {
-            if (EXP_NOTOK_SINGLE_QUOTE_STRING.test(str.slice(m.index, m.index + m[1].length))) {
-                continue;
-            }
-            if (m[1].length === 2) { // ——————————————————————————————————————— EMPTY STRING
-                continue;
-            }
-            ranges.push(composeRange(m.index, m.index + m[1].length));
+        if (EXP_NOTOK_SINGLE_QUOTE_STRING.test(str.slice(m.index, m.index + m[1].length))) {
+            continue;
         }
+        if (m[1].length === 2) { // ——————————————————————————————————————————— EMPTY STRING
+            continue;
+        }
+        ranges.push(composeRange(m.index, m.index + m[1].length));
     }
     return ranges;
 }
