df85d3d29873e029be7a3f0b2cb055b45dcd9293
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2018-12-01 13:30:03 +0100

    Added: Cor.visible to build.

--- src/utils.browser.js
+++ src/utils.browser.js
@@ -1,6 +1,7 @@
 import './utils.common';
 
 import '../utils/4_browser/Cookie.get';
+import '../utils/4_browser/Cor.visible';
 import '../utils/4_browser/Cor.xhr';
 import '../utils/4_browser/CustomEvent.create';
 import '../utils/4_browser/Element.prototype.matches';
