e95303b3326dda9c1720e52fd0acf2cf855fd103
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-02-14 09:44:18 +0100

    Added: uid() implementation.

--- utils/2_core/Cor.uid.js
+++ utils/2_core/Cor.uid.js
@@ -1,7 +1,13 @@
 import world from '../world';
 
-export default function uid() {
-
+export default function uid(len) {
+    if (!Number.isInteger(len) || len <= 0) throw new Error('api-len');
+    var set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-0123456789';
+    var b = set[Math.floor(Math.random() * (set.length - 12))];
+    while (--len) {
+        b += set[Math.floor(Math.random() * set.length)];
+    }
+    return b;
 }
 if (!world.Cor) world.Cor = {};
 world.Cor.uid = uid;
