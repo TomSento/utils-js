538846f5029c31c22a56fa5de61ae52d7482aa46
Tomas Sentkeresty <tomas.sentkeresty@goodrequest.com>
2019-02-19 22:45:17 +0100

    Added: buildTmp() to node sources.

--- src/utils.node.js
+++ src/utils.node.js
@@ -7,5 +7,6 @@ export { default as handleRequest } from '../utils/3_node/server/Cor.handleReque
 export { default as request } from '../utils/3_node/server/Cor.request';
 export { default as route } from '../utils/3_node/server/Cor.route';
 
+import '../utils/3_node/Cor.buildTmp';
 export { default as compileSrc } from '../utils/3_node/Cor.compileSrc';
 import '../utils/3_node/Cor.loadComponents';
